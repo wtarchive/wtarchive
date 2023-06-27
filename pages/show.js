081611beda78e0b47fb21ba9cf9f02bf20109478
wtarchive <comingoutamycage@gmail.com>
2023-06-27 20:51:47 +1000

    Added more books, and improved publication ID matching

--- pages/show.js
+++ pages/show.js
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router';
+import React, { useEffect, useRef } from "react";
 import convertDirectoryToJson from "../lib/convertDirectoryToJson";
 import findPath from "../lib/findPath";
+import PDFObject from "pdfobject";
 
 export default function ShowFile({ fileTree }) {
     const router = useRouter();
-    let { pubId } = router.query;
+    let { pubId, query } = router.query;
 
     if (!pubId) {
         return <div>Loading...</div>;
@@ -13,18 +15,72 @@ export default function ShowFile({ fileTree }) {
         pubId = pubId.replace(/\.pdf$/, "");
 
 
-    const filePath = findPath(fileTree, pubId);//?.replace("public", "");
+    let filePath = findPath(fileTree, pubId);
+    while(filePath == null){
+        let year = undefined;
+        let idWithoutYear = undefined;
+        if (pubId.match(/^\d{4}_/)) {
+            //Remove YYYY_
+            year = pubId.match(/^\d{4}/)[0];
+            idWithoutYear = pubId.replace(/^\d{4}_/, "");
+        }
+        if(idWithoutYear){
+            filePath = findPath(fileTree, idWithoutYear);
+            if(filePath) break;
+            filePath = findPath(fileTree, idWithoutYear + "_" + year);
+            if(filePath) break;
+            filePath = findPath(fileTree, year + "_" + idWithoutYear);
+        }
+        if(filePath) break;
+        filePath = findPath(fileTree, idWithoutYear || pubId, (a, b) => a.includes(b));
+        break;
+    }
 
     if (!filePath) {
         return <div>File not found</div>;
     }
 
+    let filename = filePath.substring(filePath.lastIndexOf("/") + 1);
+    const url = filePath;
+    // let host = window.location.hostname;
+    // if (host != "localhost") 
+    // {
+    //     if (host == "localhost")
+    //         host = "wt-archive.netlify.app";
+    //     url = "https://docs.google.com/viewer?embedded=true&url=" + encodeURIComponent(`https://${host}/${filePath}`);
+    //     if(query)
+    //         url += `&query=${encodeURIComponent(query)}`;
+    // }
+    // return (
+    //   <div>
+    //     <h1>
+    //       Displaying: <a href={filePath}>{filename}</a>
+    //     </h1>
+    //     {/* <embed src={filePath} type="application/pdf" width="100%" height="600px" style={{ height: "90vh" }} ></embed> */}
+    //     {/* <iframe src={url} width="100%" height="600px" style={{ height: "90vh" }}></iframe> */}
+    //     PDFObject.embed(pdfUrl, containerRef.current, options);
+    //   </div>
+    // );
+
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+      const pdfUrl = url;
+      const options = {
+        width: "100%",
+        height: "90vh",
+        //style: { height: "90vh" }
+      };
+
+      PDFObject.embed(pdfUrl, containerRef.current, options);
+    }, []);
+
     return (
       <div>
         <h1>
-          Displaying: <a href={filePath}>{pubId}.pdf</a>
+          Displaying: <a href={filePath}>{filename}</a>
         </h1>
-        <embed src={filePath} type="application/pdf" width="100%" height="600px" style={{ height: "90vh" }} />
+        <div ref={containerRef}></div>
       </div>
     );
 }
